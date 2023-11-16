327151b31d53e61c2f0701deaf69dbf33effa5f5
ramsankarAloor <ramsankar.aloor@gmail.com>
2023-11-16 23:05:05 +0530

    members list made neater

--- public/group-info/index.js
+++ public/group-info/index.js
@@ -30,26 +30,40 @@ function addToMembers(singleMember, adminStatus) {
   const groupMembersDiv = document.getElementById("group-members-div");
   const singleMemberDiv = document.createElement("div");
   singleMemberDiv.className = "single-member-div";
-  const textDiv = document.createElement("div");
-  textDiv.innerHTML = `<b>${singleMember.user.name}</b>&nbsp;&nbsp;&nbsp;${singleMember.user.email}&nbsp;&nbsp;&nbsp;`;
-  singleMemberDiv.appendChild(textDiv);
-
+  const nameDiv = document.createElement("div");
+  const emailDiv = document.createElement("div");
+  nameDiv.innerHTML = `<b>${singleMember.user.name}</b>`;
+  emailDiv.innerHTML = `${singleMember.user.email}`;
+  nameDiv.classList = "text-div";
+  emailDiv.className = "text-div";
+  singleMemberDiv.appendChild(nameDiv);
+  singleMemberDiv.appendChild(emailDiv);
+  
   if (adminStatus) {
     const removeButtonDiv = document.createElement("div");
+    removeButtonDiv.className = "remove-button-div";
     const removeButton = document.createElement("button");
     removeButton.textContent = "Remove";
     removeButtonDiv.appendChild(removeButton);
 
+    removeButton.addEventListener("click", removeMember);
+
+    const isMemberAdmin = singleMember.admin;
+
     const makeAdminButtonDiv = document.createElement("div");
-    const makeAdminButton = document.createElement("button");
-    makeAdminButton.textContent = "Make admin";
-    makeAdminButtonDiv.appendChild(makeAdminButton);
+    makeAdminButtonDiv.className = "make-admin-button-div"
+    if (isMemberAdmin) {
+      makeAdminButtonDiv.innerHTML = `<b>Admin</b>`;
+    } else {
+      const makeAdminButton = document.createElement("button");
+      makeAdminButton.textContent = "Make admin";
+      makeAdminButtonDiv.appendChild(makeAdminButton);
+
+      makeAdminButton.addEventListener("click", makeAdmin);
+    }
 
     singleMemberDiv.appendChild(makeAdminButtonDiv);
     singleMemberDiv.appendChild(removeButtonDiv);
-
-    removeButton.addEventListener("click", removeMember);
-    makeAdminButton.addEventListener("click", makeAdmin);
 
     async function makeAdmin() {
       const token = localStorage.getItem("token");
@@ -57,11 +71,15 @@ function addToMembers(singleMember, adminStatus) {
       const memberId = singleMember.user.id;
       const obj = { groupId, memberId };
 
-      const {data} = await axios.post(`${baseurl}/group-info/make-admin`, obj, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const { data } = await axios.post(
+        `${baseurl}/group-info/make-admin`,
+        obj,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
 
       console.log(data);
     }
